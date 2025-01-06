b6f9bcbec34721a91415f290b7e2655afa531af0
shalanah <shalanahfaith@gmail.com>
2025-01-06 15:39:01 -0600

    move build logic to tsup.config

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,10 +3,21 @@ import { defineConfig } from "tsup";
 export default defineConfig([
   {
     entry: ["src/index.ts"],
-    format: ["cjs", "esm", "iife"], // Build for commonJS and ESmodules + browser-compatible build
+    format: ["cjs", "esm"], // Build for commonJS and ESmodules + browser-compatible build
     dts: true,
     sourcemap: true,
     splitting: false,
     clean: true,
+    outDir: "dist",
+  },
+  {
+    entry: ["src/index.ts"],
+    format: ["iife"], // Build for commonJS and ESmodules + browser-compatible build
+    dts: true,
+    outDir: "dist",
+    sourcemap: true,
+    splitting: false,
+    clean: true,
+    minify: true,
   },
 ]);
