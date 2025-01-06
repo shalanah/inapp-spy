fc77459f7ec510eb218b434df59ce483dcb59149
shalanah <shalanahfaith@gmail.com>
2025-01-06 13:17:13 -0600

    add browser buid for future cdn hosting ability

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["cjs", "esm"], // Build for commonJS and ESmodules
+  format: ["cjs", "esm", "iife"], // Build for commonJS and ESmodules + browser-compatible build
   dts: true, // Generate declaration file (.d.ts)
   splitting: false,
   sourcemap: true,
   clean: true,
+  globalName: "InAppSpy", // Global variable for the UMD build
 });
