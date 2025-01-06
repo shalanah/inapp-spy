19f6ed881feeb47714194e5f9c06969510a0cbf4
shalanah <shalanahfaith@gmail.com>
2025-01-06 15:22:51 -0600

    keep unminified for now

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["cjs", "esm", "iife"], // Build for commonJS and ESmodules + browser-compatible build
-  dts: true,
-  sourcemap: true,
-  splitting: false,
-  clean: true,
-});
+export default defineConfig([
+  {
+    entry: ["src/index.ts"],
+    format: ["cjs", "esm", "iife"], // Build for commonJS and ESmodules + browser-compatible build
+    dts: true,
+    sourcemap: true,
+    splitting: false,
+    clean: true,
+  },
+]);
